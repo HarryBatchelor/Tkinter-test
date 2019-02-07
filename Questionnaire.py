764bf0bd3ef985e9b97b8513b809d900bf7e0836
HarryBatchelor <44192565+HarryBatchelor@users.noreply.github.com>
2019-02-07 16:33:11 +0000

    Update Questionnaire.py

--- Questionnaire.py
+++ Questionnaire.py
@@ -1,10 +1,30 @@
-from tkinter import *
+from Tkinter import *
 
+#GUI setup
 class Questionnaire(Frame):
-    def __init__(self, master):
+    def __init__(self, master ):
         Frame.__init__(self,master)
         self.grid()
+        self.creatProgSelect()
 
+    def creatProgSelect(self):
+        lblProg = Label(self, text='Numbers:', font=('MS', 8, 'bold'))
+        lblProg.grid(row=0, column=0, columnspan=2, sticky=NE)
+
+        self.listProg = Listbox(self, height =10)
+        scroll = Scrollbar(self, command = self.listProg.yview)
+        self.listProg.configure(yscrollcommand = scroll.set)
+
+        self.listProg.grid(row=0, column=2, columnspan=2, sticky=NE)
+        scroll.grid(row=0, column=4, sticky=W)
+
+        for item in range(1000):
+            self.listProg.insert(END, item)
+
+        self.listProg.selection_set(END)
+
+
+#Main
 root = Tk()
 root.title("Teamwork Questionnaire")
 app = Questionnaire(root)
