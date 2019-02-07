d26c93ed19453b8cac556a2b257c9f35eded12b0
HarryBatchelor <batchelorh@cardiff.ac.uk>
2019-02-07 16:44:50 +0000

    Hate this

--- Questionnaire.py
+++ Questionnaire.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 
 #GUI setup
 class Questionnaire(Frame):
@@ -8,17 +8,17 @@ class Questionnaire(Frame):
         self.creatProgSelect()
 
     def creatProgSelect(self):
-        lblProg = Label(self, text='Numbers:', font=('MS', 8, 'bold'))
+        lblProg = Label(self, text='Degree Programme:', font=('MS', 8, 'bold'))
         lblProg.grid(row=0, column=0, columnspan=2, sticky=NE)
 
-        self.listProg = Listbox(self, height =10)
-        scroll = Scrollbar(self, command = self.listProg.yview)
+        self.listProg = Listbox(self, height =3)
+        scroll = Scrollbar(self, command =self.listProg.yview)
         self.listProg.configure(yscrollcommand = scroll.set)
 
         self.listProg.grid(row=0, column=2, columnspan=2, sticky=NE)
         scroll.grid(row=0, column=4, sticky=W)
 
-        for item in range(1000):
+        for item in ["CS", "CS with", "BIS", "SE", "Joints", ""]:
             self.listProg.insert(END, item)
 
         self.listProg.selection_set(END)
